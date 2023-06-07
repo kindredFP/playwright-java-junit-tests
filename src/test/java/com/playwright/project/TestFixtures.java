16c679072371ec80186daec3300a175886dbef48
Francis <kindredfp@users.noreply.github.com>
2023-06-06 23:24:26 -0400

    make it headless

--- src/test/java/com/playwright/project/TestFixtures.java
+++ src/test/java/com/playwright/project/TestFixtures.java
@@ -15,7 +15,7 @@ class TestFixtures {
     @BeforeAll
     void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @AfterAll
