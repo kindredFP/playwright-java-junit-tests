b5b2f80b88b02beac095df5d83d65b950cf9492c
Francis <kindredfp@users.noreply.github.com>
2023-06-06 23:18:33 -0400

    Updated pom and added base test class

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 DESCRIPTION:
 Boiler plate implementation of Playwright tests built with Java and JUnit testrunner.
 
+Maven syntax
+exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D "exec.args=install --with-deps"
+
 REFERENCES USED:
 https://playwright.dev
---
+++ src/test/java/com/playwright/project/TestFixtures.java
@@ -0,0 +1,49 @@
+package com.playwright.project;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.Paths;
+
+// Subclasses will inherit PER_CLASS behavior.
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TestFixtures {
+    // Shared between all tests in the class.
+    Playwright playwright;
+    Browser browser;
+
+    @BeforeAll
+    void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @AfterAll
+    void closeBrowser() {
+        playwright.close();
+    }
+
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+
+        // Start tracing before creating / navigating a page.
+        context.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true)
+                .setSources(true));
+
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.tracing().stop(new Tracing.StopOptions()
+                .setPath(Paths.get("trace" + context.hashCode() + ".zip")));
+        context.close();
+    }
+}
